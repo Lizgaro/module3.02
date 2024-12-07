aa0e4766f1f3e057d59e98a7a3b2ed6695965166
Lizgaro <lizgaro13@gmail.com>
2024-12-07 21:23:58 +1000

    Add files via upload
    Домашняя работа по уроку "Способы вызова функции"

---
+++ 3.02.py
@@ -0,0 +1,32 @@
+import re
+
+
+# Функция для проверки валидности email
+def is_valid_email(email):
+    return bool(re.match(r'^[\w._%+-]+@[a-zA-Z0-9.-]+\.(com|ru|net)$', email))
+
+
+# Функция send_email
+def send_email(message, recipient, *, sender="university.help@gmail.com"):
+    # Проверка на корректность email
+    if not is_valid_email(sender) or not is_valid_email(recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+
+    # Проверка на отправку самому себе
+    if sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+        return
+
+    # Проверка на отправителя по умолчанию
+    if sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    else:
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+
+
+# Примеры выполнения программы:
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
